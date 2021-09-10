fb81efe4f452211b8587cdb2998b426cb8f38ee2
Arthur <arthurbokhanqa@gmail.com>
2021-09-10 17:56:26 +0300

    objects and work with

---
+++ js/objects.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        background: 'red'
+    },
+    makeTest: function(){
+        console.log('test');
+    }
+};
+
+console.log(Object.keys(options).length);
+options.makeTest();
+
+// delete options.name;
+
+let counter = 0;
+
+for (let key in options) {
+    if (typeof (options[key]) == 'object') {
+        for (let i in options[key]) {
+            console.log(`Property  ${i} is ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Property ${key} is ${options[key]}`);
+        counter++;
+    }
+}
+
+console.log(counter);
