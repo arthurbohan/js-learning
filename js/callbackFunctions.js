dd377d20da152ebdf8b70aebf6e21415b378a674
Arthur <arthurbokhanqa@gmail.com>
2021-09-10 13:08:54 +0300

    callbackFunctions

---
+++ js/callbackFunctions.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function first(){
+    setTimeout(function(){
+        console.log(1);
+
+    }, 500);
+}
+
+function second(){
+    console.log(1);
+}
+
+first();
+second();
+
+function learnJS(lang, callback){
+    console.log(`Im lerning ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('I ended this lesson');
+}
+
+learnJS('JavaScript', function(){
+    console.log('I ended this lesson');
+});
