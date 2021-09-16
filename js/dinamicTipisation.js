7536ca75a8eaee10ea88be95376b643675f2f6a0
Arthur <arthurbokhanqa@gmail.com>
2021-09-16 15:39:45 +0300

    correct code

--- js/dinamicTipisation.js
+++ js/dinamicTipisation.js
@@ -2,11 +2,11 @@
 
 // to string
 // 1
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
+console.log(typeof (String(null)));
+console.log(typeof (String(4)));
 
 // 2
-console.log(typeof(String(5 + '')));
+console.log(typeof (String(5 + '')));
 
 const num = 5;
 console.log('https://vk.com/catalog/' + num);
@@ -14,13 +14,13 @@ const fotSize = 26 + 'px';
 
 // to number
 // 1
-console.log(typeof(Number('4')));
+console.log(typeof (Number('4')));
 
 //2 
-console.log(typeof(+'5'));
+console.log(typeof (+'5'));
 
 //3
-console.log(typeof(parseInt('15px', 10)));
+console.log(typeof (parseInt('15px', 10)));
 
 let answ = +prompt('Hello', '');
 
@@ -30,20 +30,20 @@ let answ = +prompt('Hello', '');
 // 1
 let switcher = null;
 
-if (switcher){
+if (switcher) {
     console.log('working..');
 }
 
 switcher = 1;
 
-if (switcher){
+if (switcher) {
     console.log('working..');
 }
 
 // 2
-console.log(typeof(Boolean('4')));
+console.log(typeof (Boolean('4')));
 
 //3
-console.log(typeof(!!'4444'));
+console.log(typeof (!!'4444'));
 
 
