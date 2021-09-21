11319bf9b8719740b98fb82d4b6e4a1f6d78dda9
Arthur <arthurbokhanqa@gmail.com>
2021-09-21 22:44:49 +0300

    practice update 30

--- project/js/script.js
+++ project/js/script.js
@@ -25,9 +25,9 @@ const movieDB = {
 };
 
 const adv = document.querySelectorAll('.promo__adv img'),
-      poster = document.querySelector('.promo__bg'),
-      genre = poster.querySelector('.promo__genre'),
-      films = document.querySelectorAll('.promo__interactive-list');
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
 
 adv.forEach(item => {
     item.remove();
@@ -36,5 +36,13 @@ adv.forEach(item => {
 genre.textContent = ('Драма');
 poster.style.backgroundImage = 'url("img/bg.jpg")';
 
+movieList.innerHTML = "";
+movieDB.movies.sort();
 
-
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
